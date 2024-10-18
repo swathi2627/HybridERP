5faaedc7b517e59bb25b92df4888fb5e17a028a7
SaanviNaveen <SaanviNaveen@Saanvi>
2024-10-18 17:51:34 +0530

    18th oct code checkin

--- src/test/java/commonFunctions/FunctionLibrary.java
+++ src/test/java/commonFunctions/FunctionLibrary.java
@@ -1,5 +1,98 @@
 package commonFunctions;
 
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+
 public class FunctionLibrary {
+	public static WebDriver driver;
+	public static Properties property;
+
+	public static WebDriver startBrowser() throws Throwable {
+		property = new Properties();
+		property.load(new FileInputStream("./PropertyFiles/Environment.properties"));
+
+		if (property.getProperty("Browser").equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+
+		} else if (property.getProperty("Browser").equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+		} else {
+			Reporter.log("browser value not found ....", true);
+		}
+
+		return driver;
+	}
+
+	public static void openUrl() {
+		driver.get(property.getProperty("Url"));
+	}
+
+	public static void waitForElement(String LocatorType, String LocatorValue, String TestData) {
+
+		WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(TestData)));
+
+		if (LocatorType.equalsIgnoreCase("xpath")) {
+			explicitWait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(LocatorValue)));
+		}
+		if (LocatorType.equalsIgnoreCase("name")) {
+			explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.name(LocatorValue)));
+		}
+		if (LocatorType.equalsIgnoreCase("id")) {
+			explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(LocatorValue)));
+		}
+	}
+
+	public static void typeAction(String LocatorType, String LocatorValue, String TestData) {
+		if (LocatorType.equalsIgnoreCase("xpath")) {
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		}
+		if (LocatorType.equalsIgnoreCase("id")) {
+			driver.findElement(By.id(LocatorValue)).clear();
+			driver.findElement(By.id(LocatorValue)).sendKeys(TestData);
+		}
+		if (LocatorType.equalsIgnoreCase("name")) {
+			driver.findElement(By.name(LocatorValue)).clear();
+			driver.findElement(By.name(LocatorValue)).sendKeys(TestData);
+		}
+	}
+
+	public static void clickAction(String LocatorType, String LocatorValue) {
+		if (LocatorType.equalsIgnoreCase("xpath")) {
+			driver.findElement(By.xpath(LocatorValue)).click();
+		}
+		if (LocatorType.equalsIgnoreCase("id")) {
+			driver.findElement(By.id(LocatorValue)).click();
+		}
+		if (LocatorType.equalsIgnoreCase("name")) {
+			driver.findElement(By.name(LocatorValue)).click();
+		}
+	}
+
+	public static void validateTitle(String ExpectedTitle) {
+		String actualTitle = driver.getTitle();
+		try {
+			Assert.assertEquals(actualTitle, ExpectedTitle, "not matching");
+		} catch (AssertionError e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void closeBrowser() {
+		driver.quit();
+	}
 
 }
--- src/test/java/driverFactory/DriverScript.java
+++ src/test/java/driverFactory/DriverScript.java
@@ -1,5 +1,86 @@
 package driverFactory;
 
+import org.openqa.selenium.WebDriver;
+
+import commonFunctions.FunctionLibrary;
+import utilities.ExcelFileUtilities;
+
 public class DriverScript {
+	WebDriver driver;
+	String inputpath = "./FileInput/Controller.xlsx";
+	String outputpath = "./Fileoutput/Hybridresults.xlsx";
+	String TCSheet = "MasterTestCases";
+
+	public void startTest() throws Throwable {
+
+		String Module_Stsus = "";
+		String Module_New = "";
+
+		ExcelFileUtilities excel = new ExcelFileUtilities(inputpath);
+
+		int row = excel.rowCount(TCSheet);
+		for (int i = 1; i <= row; i++) {
+
+			if (excel.getCellData(TCSheet, i, 2).equalsIgnoreCase("y")) {
+				excel.setCellData(TCSheet, i, 3, "", outputpath);
+				String TCModule = excel.getCellData(TCSheet, i, 1);
+
+				// irerating rows from TCModule sheet
+
+				for (int j = 1; j <= excel.rowCount(TCModule); j++) {
+
+					// read all cells
+
+					String Description = excel.getCellData(TCModule, j, 0);
+					String ObjType = excel.getCellData(TCModule, j, 1);
+					String LocatorType = excel.getCellData(TCModule, j, 2);
+					String LocatorValue = excel.getCellData(TCModule, j, 3);
+					String TestData = excel.getCellData(TCModule, j, 4);
+
+					try {
+						if (ObjType.equalsIgnoreCase("startBrowser")) {
+							driver = FunctionLibrary.startBrowser();
+						}
+						if (ObjType.equalsIgnoreCase("openUrl")) {
+							FunctionLibrary.openUrl();
+						}
+						if (ObjType.equalsIgnoreCase("waitForElement")) {
+							FunctionLibrary.waitForElement(LocatorType, LocatorValue, TestData);
+						}
+						if (ObjType.equalsIgnoreCase("typeAction")) {
+							FunctionLibrary.typeAction(LocatorType, LocatorValue, TestData);
+						}
+						if (ObjType.equalsIgnoreCase("clickAction")) {
+							FunctionLibrary.clickAction(LocatorType, LocatorValue);
+						}
+						if (ObjType.equalsIgnoreCase("validateTitle")) {
+							FunctionLibrary.validateTitle(TestData);
+						}
+						if (ObjType.equalsIgnoreCase("closeBrowser")) {
+							FunctionLibrary.closeBrowser();
+						}
+
+						excel.setCellData(TCModule, j, 5, "pass", outputpath);
+						Module_Stsus = "true";
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+						excel.setCellData(TCModule, j, 5, "fail", outputpath);
+						Module_New = "false";
+					}
+					if (Module_Stsus.equalsIgnoreCase("true")) {
+						excel.setCellData(TCSheet, i, 3, "pass", outputpath);
+					}
+				}
+					if (Module_New.equalsIgnoreCase("false")) {
+                          excel.setCellData(TCSheet, i, 3, "fail", outputpath);
+					}
+				
+			}
+
+			else {
+				excel.setCellData(TCSheet, i, 3, "Blocked", outputpath);
+			}
+		}
+	}
 
 }
